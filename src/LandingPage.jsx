a51c2e1e83dc1af0a257655d801beea5a8dddf30
Mcdemzy <adeyemoademola12@gmail.com>
2024-01-08 11:36:35 +0100

    Fixing Signin bug

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/navbar/Navbar";
 import Design from "./components/design/Design";
 import Services from "./components/services/Services";
@@ -6,6 +8,7 @@ import Works from "./components/works/Works";
 import Banner from "./components/banner/Banner";
 import Testimonials from "./components/testimonials/Testimonials";
 import Footer from "./components/footer/Footer";
+import Signup from "./components/signup/Signup";
 
 const App = () => {
   return (
@@ -17,6 +20,11 @@ const App = () => {
       <Banner />
       <Testimonials />
       <Footer />
+
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   );
 };
