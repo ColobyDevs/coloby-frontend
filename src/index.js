dc99e4fa1f239d14b3728cef5fbe75f165e64079
Mcdemzy <adeyemoademola12@gmail.com>
2024-01-08 11:30:57 +0100

    Installed react router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "./LandingPage";
+import { BrowserRouter } from "react-router-dom";
 import "./landingpage.css";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
