06dafd98aa9a9732d939026107c6b12100f3206b
Mayowa Ayeni <mayhoral008@gmail.com>
2024-06-03 18:17:30 +0100

    added task properties update functionalities

--- src/taskboard/taskView.js
+++ src/taskboard/taskView.js
@@ -1,59 +1,100 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useReducer, useEffect } from "react";
 import { HiMiniXMark } from "react-icons/hi2";
 import { RiBallPenLine } from "react-icons/ri";
 import { Context } from "../context/context";
-import TextEditor from "../shared/textEditor";
-import FroalaEditorView from "react-froala-wysiwyg/FroalaEditorView";
+import { toast } from "sonner";
+import CheckListBoard from "../shared/checkListBoard";
 
 const TaskView = () => {
-  const { modal, taskBoard, rooms, auth, textEditorContent } = useContext(Context);
+  const { modal, taskBoard, rooms, auth, textEditorContent, loader } =
+    useContext(Context);
   const { token } = auth;
+  const { setIsLoading } = loader;
   const { roomsList } = rooms;
   const { viewTaskModal, setViewTaskModal } = modal;
-  const {editorText} = textEditorContent
   const {
     taskInView,
     activeRoomId,
-    taskUpdate,
+    setActiveTasks,
     setTaskInView,
-    activeTasks,
   } = taskBoard;
   const [editMode, setEditMode] = useState(false);
   
+  const [updateForm, setUpdateForm] = useState({
+    title: "",
+    due_date: "",
+    description: "",
+  });
 
+  useEffect(() => {
+    setUpdateForm({
+      title: taskInView.title,
+      due_date: taskInView.due_date,
+      description: taskInView.description,
+    });
+  }, [taskInView]);
+
+  const handleUpdate = (e) => {
+    setUpdateForm((prevForm) => {
+      return { ...prevForm, [e.target.name]: e.target.value };
+    });
+  };
   const viewTaskHandler = () => {
     setViewTaskModal(false);
     setEditMode(false);
+    setUpdateForm({
+      title: taskInView.title,
+      due_date: taskInView.due_date,
+      description: taskInView.description,
+    });
+    setDurEditMode(false);
   };
 
-  const handleEditMode = (type) => {
-    type === "edit" && setEditMode(true);
-    type === "update" && setEditMode(false);
-
+  const updateTask = () => {
     const updateFunc = async () => {
-      const response = await fetch(
-        `https://coloby.onrender.com/api/v1/room/${roomsList[activeRoomId].slug}/tasks/${taskInView.id}/`,
-        {
-          method: "PATCH",
-          // taskUpdate from textEditor.js
-          body: JSON.stringify(taskUpdate), 
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token,
-          },
-        }
-      );
-      const responseData = await response.json();
-      console.log(responseData);
-     
-      setTaskInView(responseData);
-      activeTasks.map((task, i) => {
-        if (task.id === responseData.id) {
-           return task.description = responseData.description
-        }
-      });
+      try {
+        setIsLoading(true);
+
+        const response = await fetch(
+          `https://coloby.onrender.com/api/v1/room/${roomsList[activeRoomId].slug}/tasks/${taskInView.id}/`,
+          {
+            method: "PATCH",
+            // taskUpdate from textEditor.js
+            body: JSON.stringify(updateForm),
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+        const responseData = await response.json();
+        console.log(responseData);
+
+        setTaskInView(responseData);
+
+        setActiveTasks((prevTasks) =>
+          prevTasks.map((task) =>
+            task.id === responseData.id
+              ? {
+                  ...task,
+                  due_date: responseData.due_date,
+                  title: responseData.title,
+                  description: responseData.description,
+                }
+              : task
+          )
+        );
+
+        setIsLoading(false);
+        toast.success("Task updated!");
+      } catch (err) {
+        setIsLoading(false);
+        console.log(err.message);
+      }
     };
-    type === "update" && editorText !== taskInView.description && editorText !== '' && updateFunc();
+    updateForm.description !== taskInView.description ||
+      updateForm.title !== taskInView.title ||
+      (updateForm.due_date !== taskInView.due_date && updateFunc());
   };
 
   const formatDate = (date) => {
@@ -66,52 +107,111 @@ const TaskView = () => {
     return formattedDate;
   };
 
+  const [titleShow, setTitleShow] = useState(false);
+  const [descShow, setDescShow] = useState(false);
+  const handleTitleShow = () => {
+    setTitleShow(!titleShow);
+  };
+
+  const [showDur, setShowDur] = useState(false);
+  const handleShowDur = () => {
+    setShowDur(!showDur);
+  };
+
+  const handleDescShow = () => {
+    setDescShow(!descShow);
+  };
+  const [durEditMode, setDurEditMode] = useState(false);
+  const handleDurEditMode = () => {
+    setDurEditMode(!durEditMode);
+  };
+
   if (viewTaskModal) {
     return (
       <main className="  fixed top-0 bottom-0 text-sm right-0 left-0 flex z-40 items-center justify-center modal">
-        <section className="w-[60%] h-[90%]  bg-white ">
+        <section className="w-[60%] h-[90%]  bg-white overflow-auto">
           <div className="flex flex-row justify-between px-4 items-center shadow-sm  bg-[#e7e7e7] h-[8%]">
-            <h2>{taskInView.title}</h2>
+            <div className="w-3/12  flex flex-row items-center space-x-2">
+              {!titleShow ? (
+                <h2 onClick={handleTitleShow}>{taskInView.title}</h2>
+              ) : (
+                <input
+                  name="title"
+                  className="bg-[#e7e7e7]"
+                  value={updateForm.title}
+                  onChange={handleUpdate}
+                />
+              )}
+            </div>
             <HiMiniXMark
               className="text-lg cursor-pointer"
               onClick={viewTaskHandler}
             />
           </div>
           <section className="flex flex-row w-full h-full">
-            <article className="w-[70%] pt-10 px-4 flex flex-col space-y-5">
-              <div className="flex flex-row space-x-2">
+            <article className="w-[70%] pt-2 px-4 flex flex-col space-y-1">
+              <div
+                onMouseEnter={handleShowDur}
+                onMouseLeave={handleShowDur}
+                className="flex flex-row items-center space-x-2"
+              >
                 <h2>Project Period:</h2>
-                <span>{`${formatDate(taskInView.created_at)} to ${formatDate(
-                  taskInView.due_date
-                )}`}</span>
-                <RiBallPenLine className="cursor-pointer" />
-              </div>
-              <div>
-                <div className="flex flex-row justify-between">
-                  <h2>Description</h2>
-                  <RiBallPenLine
-                    className="cursor-pointer"
-                    onClick={() => handleEditMode("edit")}
+                <span className=" text-xs">
+                  {formatDate(taskInView.created_at)} to
+                </span>
+                {!durEditMode ? (
+                  <span className="text-xs">
+                    {formatDate(taskInView.due_date)}
+                  </span>
+                ) : (
+                  <input
+                    className=" px-2 rounded-sm text-xs border border-solid"
+                    name="due_date"
+                    type="date"
+                    onChange={handleUpdate}
+                    value={updateForm.due_date}
                   />
+                )}
+                {showDur && <RiBallPenLine onClick={handleDurEditMode} />}
+              </div>
+              <div></div>
+              <div>
+                <div className="flex flex-row items-center space-x-2 ">
+                  <h2>Description</h2>
                 </div>
                 <div className="">
-                  {!editMode ? (
-                    <div className="text-xs pt-2 h-32 rounded-none desc">
-                      <FroalaEditorView model={taskInView.description} />
+                  {!descShow ? (
+                    <div onClick={handleDescShow} className="text-xs pt-2">
+                      {taskInView.description}
                     </div>
-                  ) : 
-                  (
-                    <TextEditor />
-                  )
-                  // <div contentEditable>{taskInView.description}</div>
-                  }
+                  ) : (
+                    <input
+                      name="description"
+                      className="text-xs pt-2"
+                      value={updateForm.description}
+                      onChange={handleUpdate}
+                    />
+                  )}
+                </div>
+                <div className="mt-4 ">
+                  <CheckListBoard />
                 </div>
                 <button
-                  onClick={() => handleEditMode("update")}
-                  disabled = {editorText === taskInView.description || editorText === '' }
-                  className={` ${
-                    !editMode && "invisible"
-                  }  block ml-auto ${editorText === taskInView.description || editorText === '' ? 'bg-slate-300' : 'bg-[#052CFD]'} text-white h-8 w-28 mt-2 rounded-md`}
+                  onClick={() => updateTask}
+                  disabled={
+                    !(
+                      updateForm.description !== taskInView.description ||
+                      updateForm.title !== taskInView.title ||
+                      updateForm.due_date !== taskInView.due_date
+                    )
+                  }
+                  className={` block ml-auto ${
+                    updateForm.description !== taskInView.description ||
+                    updateForm.title !== taskInView.title ||
+                    updateForm.due_date !== taskInView.due_date
+                      ? "bg-[#052CFD]"
+                      : "bg-slate-300"
+                  } text-white h-8 w-28 mt-2 rounded-md`}
                 >
                   Update
                 </button>
